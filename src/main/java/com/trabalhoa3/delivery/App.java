0ad1524306f3792ae8ed9d7354fad3a7ec9a3f95
Patrick Luz <patrickluz.jobs@gmail.com>
2022-06-01 16:47:17 -0300

    feat: add Database class

---
+++ src/main/java/com/trabalhoa3/delivery/App.java
@@ -0,0 +1,7 @@
+package main.java.com.trabalhoa3.delivery;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+    }
+}
---
+++ src/main/java/com/trabalhoa3/delivery/database/Database.java
@@ -0,0 +1,46 @@
+package main.java.com.trabalhoa3.delivery.database;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import main.java.com.trabalhoa3.delivery.database.seed.Seeder;
+import main.java.com.trabalhoa3.delivery.util.FileIO;
+
+public class Database {
+    private final Path PATH = Paths.get("src", "main", "java", "com", "trabalhoa3", "delivery", "database", "files");
+    private final String FILE_EXTENSION_TYPE = ".csv";
+
+    public Database() {
+        Seeder sd = new Seeder();
+    }
+
+    public void writeFile(Path filePath, String[] data) {
+        FileIO.writeFile(filePath, data);
+    }
+
+    public List<List<String>> readFile(String filePath) throws FileNotFoundException, IOException {
+        return FileIO.readFile(filePath);
+    }
+
+    public Path getPath(String filePath) {
+        Path path = Paths.get(this.PATH.toAbsolutePath().toString(), filePath.concat(this.FILE_EXTENSION_TYPE));
+        return path;
+    }
+
+    public Path getPath() {
+        return this.PATH;
+    }
+
+    public String getPathToString(String filePath) {
+        Path path = Paths.get(this.PATH.toAbsolutePath().toString(), filePath.concat(this.FILE_EXTENSION_TYPE));
+        return path.toAbsolutePath().toString();
+    }
+
+    public String getPathToString() {
+        return this.PATH.toAbsolutePath().toString();
+    }
+
+}

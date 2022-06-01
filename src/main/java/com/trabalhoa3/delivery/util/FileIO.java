48e11aea97cca1b46b58aa455c0b376eb3677667
Patrick Luz <patrickluz.jobs@gmail.com>
2022-06-01 16:38:50 -0300

    feat: add class for handling .csv files

---
+++ src/main/java/com/trabalhoa3/delivery/util/FileIO.java
@@ -0,0 +1,46 @@
+package main.java.com.trabalhoa3.delivery.util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileIO {
+    private static final String COMMA_DELIMITER = ",";
+
+    public static void writeFile(Path filePath, String[] data) {
+        try (BufferedWriter writer = Files.newBufferedWriter(filePath,
+                StandardCharsets.UTF_8, StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE, StandardOpenOption.APPEND)) {
+            writer.write(ConvertToCsv.convert(data));
+            writer.write("\n");
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static List<List<String>> readFile(String filePath)
+            throws FileNotFoundException, IOException {
+        File file = new File(filePath);
+
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        }
+        return records;
+    }
+}

81f5dfa0d0adc8d3f1a7f31baae561dff63c89ec
Gabriel Chagas <gchagas090299@outlook.com.br>
2022-06-21 19:11:58 -0300

    refactor: alterate authclass

---
+++ src/com/trabalhoa3/delivery/domain/services/client/AuthClient.java
@@ -0,0 +1,4 @@
+package com.trabalhoa3.delivery.domain.services.client;
+
+public class AuthClient {
+}
---
+++ src/com/trabalhoa3/delivery/domain/services/client/CreateNewClient.java
@@ -0,0 +1,2 @@
+package com.trabalhoa3.delivery.domain.services.client;public class CreateNewClient {
+}
